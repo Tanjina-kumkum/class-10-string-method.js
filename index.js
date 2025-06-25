631ea51007bccb77a5b5b30e8ab00cb940d7f595
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-06-25 20:57:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+
+function showName (){
+   let name= "tanjina";
+    console.log(name);
+    
+}
+showName()
+let name1= "rup"
+function showCase() {
+    console.log(name1);
+    
+    
+}
+showCase()
+let num= 5;
+let result= num>0?' positive':'negative'
+console.log(result);
+let num1= 6;
+let result1= num1!=0 ? "positive" : "negative"
+console.log(result1);
+
+let num2= 6;
+let result2= num2===0 ? "positive" : "negative"
+console.log(result2);
+let age= 18;
+let message= age>=18 ? "can vote" : "too young to vote"
+console.log(message);
+let name = "tanjina"
+console.log(name.length);
+let que = "how are you?"
+console.log(que.length);
+let num3= "five kg rice"
+console.log(num3.length);
+
+let name2= "kumkum"
+console.log(name2.toUpperCase());
+let name3= "MUNMUN"
+console.log(name3.toLowerCase());
+let message1 = "Welcome to somaj seba Office";
+console.log(message1.includes("somaj")); 
+console.log(message1.includes('Somaj'));
+
+
+
+
+
+
+
+
+
+
